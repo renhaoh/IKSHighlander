3ea4d761bf976024f6150706fb0c14911a7c78b3
Renhao Hu <renhaohu@gmail.com>
2018-03-18 20:38:20 -0400

    remove main

--- src/main/java/hms/highlander/word/cloud/CreateWC.java
+++
@@ -1,9 +0,0 @@
-package hms.highlander.word.cloud;
-
-public class CreateWC {
-
-    public static void main(String[] args) {
-        WordCloud wordCloud = new WordCloudImpl();
-        wordCloud.generateCloud(args[0], Integer.parseInt(args[1]), Integer.parseInt(args[2]), args[3]);
-    }
-}

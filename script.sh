3da06aeee4cb4c7f29bc6ef558347dd511dc8fba
Renhao Hu <renhaohu@gmail.com>
2018-04-11 14:18:07 -0400

    word cloud update

--- script.sh
+++ script.sh
@@ -4,5 +4,5 @@ echo "Creating word cloud..."
 # Run wordCloud data
 # Input file must be called responses.txt and in the wordcloud folder
 # Writes output to wordcloud/cloud.png
-java -cp hms.highlander-1.0-all.jar hms.highlander.word.cloud.RunWordCloud
+java -cp build/libs/hms.highlander-1.0-all.jar hms.highlander.word.cloud.RunWordCloud
 echo "Done creating word cloud"
